502adde30778a71d8910c3c71ccaa6badbea3f87
Arthur Ming <guobin.ming@pwc.com>
2022-04-11 20:14:36 +0800

    feat: add react router support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import {
-  Box, Button, Flex, Image, Text,
+  Box, Button, Flex, Image, Text, Link,
 } from '@chakra-ui/react';
 import { SunIcon } from '@chakra-ui/icons';
+import { Routes, Route, Link as ReactLink } from 'react-router-dom';
 import logo from './react-logo.svg';
 import './App.scss';
 import Login from './components/Login';
+
+const Home = () => <Box>this is home</Box>;
+const Home2 = () => <Box>this is home 2</Box>;
 
 const App = () => (
   <>
     <Flex p='9px 16px' w='100%' color='white' bg='cyan.500' h={16} flexWrap='wrap' alignItems='center'>
       <Image src={logo} w='26px' h='26px' />
       <Text ml='4px'>Hoteler</Text>
-
+      <Link ml={1} as={ReactLink} to='/'>
+        Dashboard
+      </Link>
       <Box flexGrow={1} />
 
       <Button bg='cyan.500' variant='ghost' _hover={{ bg: 'cyan.500' }} _active={{ bg: 'cyan.500' }}>
@@ -20,7 +26,11 @@ const App = () => (
       </Button>
     </Flex>
     <Box as='section'>
-      <Login />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='home2' element={<Home2 />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </Box>
   </>
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import './index.scss';
 import App from './App';
@@ -11,11 +12,13 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 const app = (
   <React.StrictMode>
-    <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 root.render(app);
