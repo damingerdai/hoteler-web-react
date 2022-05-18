624a5177c5c459c2a76ae43b97e7e733cb871a3b
Arthur Ming <guobin.ming@pwc.com>
2022-05-18 11:39:34 +0800

    style: format setupProxy js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = app => {
+module.exports = (app) => {
   app.use(
     '/api',
     createProxyMiddleware({
       target: 'http://localhost:8443',
-      changeOrigin: true
-    })
+      changeOrigin: true,
+    }),
   );
 };
