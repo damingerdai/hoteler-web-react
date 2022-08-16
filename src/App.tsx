35bfb8df4a9d9ef6c57d1c13ed343f72703c28e4
Arthur Ming <guobin.ming@pwc.com>
2022-08-16 10:22:38 +0800

    refactor: add with suspense wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import GlobalLoading from './components/GlobalLoading';
 import Navbar from './components/Navbar';
+import { withSuspense } from './components/WithSuspense';
 import './App.scss';
 
 const Dashboard = React.lazy(() => import('./pages/Dashboard'));
@@ -11,19 +12,57 @@ const Room = React.lazy(() => import('./pages/Room'));
 const Login = React.lazy(() => import('./pages/Login'));
 const Forbidden = React.lazy(() => import('./pages/Forbidden'));
 
+const Layout: React.FC = () => (
+  <Routes>
+    <Route
+      path='dashboard'
+      element={(
+        withSuspense(<Dashboard />)
+      )}
+    />
+    <Route
+      path='customer'
+      element={(
+        withSuspense(<Customer />)
+      )}
+    />
+    <Route
+      path='room'
+      element={(
+        withSuspense(<Room />)
+      )}
+    />
+    <Route
+      path='login'
+      element={(
+        withSuspense(<Login />)
+      )}
+    />
+  </Routes>
+);
+
 const App = () => (
   <>
     <Navbar />
     <Box as='section'>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
-        <Route path='dashboard' element={<React.Suspense fallback={<GlobalLoading />}><Dashboard /></React.Suspense>} />
-        <Route path='customer' element={<React.Suspense fallback={<GlobalLoading />}><Customer /></React.Suspense>} />
-        <Route path='room' element={<React.Suspense fallback={<GlobalLoading />}><Room /></React.Suspense>} />
-        <Route path='loading' element={<React.Suspense fallback={<GlobalLoading />}><GlobalLoading /></React.Suspense>} />
-        <Route path='login' element={<React.Suspense fallback={<GlobalLoading />}><Login /></React.Suspense>} />
-        <Route path='403' element={<React.Suspense fallback={<GlobalLoading />}><Forbidden /></React.Suspense>} />
-        <Route path='*/*' element={<React.Suspense fallback={<GlobalLoading />}><Dashboard /></React.Suspense>} />
+        <Route path='/*' element={<Layout />} />
+        <Route
+          path='403'
+          element={(
+            <React.Suspense fallback={<GlobalLoading />}>
+              <Forbidden />
+            </React.Suspense>
+          )}
+        />
+        <Route
+          path='*/*'
+          element={(
+            <React.Suspense fallback={<GlobalLoading />}>
+              <Dashboard />
+            </React.Suspense>
+          )}
+        />
       </Routes>
     </Box>
   </>
